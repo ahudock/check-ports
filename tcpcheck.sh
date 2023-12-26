9edd624e825fc9c9f0ce92cd233dae59db9e6d72
Andy Hudock <ahudock@pm.me>
2023-12-26 08:20:12 -0800

    docs: update usage

--- tcpcheck.sh
+++ tcpcheck.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-# Checks for open ports based using tcp exit codes
+# Checks for open ports using tcp exit codes
 # @author: Andy Hudock <ahudock@pm.me>
-# Usage: ./tcpcheck.sh -h <host> -p <starting port>[-<ending port>]
+#
+# Usage:
+#   tcpcheck.sh [-h <hostname>] [-p <port>|<start_port>-<end_port>]
+#
+# Options:
+#   -h <host>   The target hostname or IP address (default: localnost)
+#
+#   -p <port>|<start_port>-<end_port>
+#               The port or port range to scan (default: 22).
 
 # Default values
 host="localhost"
